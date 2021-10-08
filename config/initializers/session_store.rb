1f5f03e11ff4caf3eadd5cae8d53345b790bdd2b
Mikelanne <mikelanne.wood@gmail.com>
2021-10-08 14:57:59 -0400

    fixing session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_wot_backend"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_wot_backend", domain: "jdh-wot-backend-api.herokuapp.com"
+else
+    Rails.application.config.session_store :cookie_store, key: "_wot_backend"
+end
