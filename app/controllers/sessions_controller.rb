48c98d9125f7b64eebccc7df4dba4e23e8424e4f
Mikelanne <mikelanne.wood@gmail.com>
2022-02-24 10:55:39 -0500

    finishing user authentication on api side

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+    include CurrentUserConcern
 
     def create
         user = User.find_by(username: params["user"]["username"])
@@ -18,33 +19,22 @@ class SessionsController < ApplicationController
         end
     end
 
-#     def is_logged_in?
-#         if logged_in? && current_user
-#             render json: {
-#                 logged_in: true,
-#                 user: current_user
-#             }
-#         else
-#             render json: {
-#                 logged_in: false,
-#                 message: 'please try again'
-#             }
-#         end
-#     end
+    def logged_in
+        if @current_user
+            render json: {
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false,
+            }
+        end
+    end
 
-#     def destroy
-#         logout!
-#         render json: {
-#             status: 200,
-#             logged_out: true
-#         }
-#     end
-
-#     private
-
-#     def session_params
-#         params.require(:user).permit(:username, :email, :password)
-#     end
-
-
-# end
+    def logout
+        reset_session
+        render json: {status: 200, logged_out: true}
+    end
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
 
-  post '/login', to: 'sessions#create'
-  post '/logout', to: 'sessions#destroy'
-  get '/logged_in', to: 'sessions#is_logged_in?'
+  # post '/login', to: 'sessions#create'
+  # post '/logout', to: 'sessions#destroy'
+  # get '/logged_in', to: 'sessions#is_logged_in?'
 
   root to: "static#home"
 
-  resources :users
-  resources :locations
-  resources :groups
-  resources :characters
+
   resources :registrations, only: [:create]
+  resources :sessions, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
