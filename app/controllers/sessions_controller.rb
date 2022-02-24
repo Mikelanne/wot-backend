0c74213e3378843b9619f40d70b12a43d47b3406
Mikelanne <mikelanne.wood@gmail.com>
2022-02-24 10:18:19 -0500

    working on user authentication again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,49 +1,50 @@
 class SessionsController < ApplicationController
 
     def create
-        @user = User.find_by(username: session_params[:username])
+        user = User.find_by(username: params["user"]["username"])
+        .try(:authenticate, params["user"]["password"])
 
-        if @user && @user.authenticate(session_params[:password])
-            login!
+        if user
+            session[:user_id] = user.id
             render json: {
+                status: :created,
                 logged_in: true,
-                user: @user
+                user: user
             }
         else
             render json: {
-                status: 401,
-                errors: ['please try again']
+                status: 401
             }
         end
     end
 
-    def is_logged_in?
-        if logged_in? && current_user
-            render json: {
-                logged_in: true,
-                user: current_user
-            }
-        else
-            render json: {
-                logged_in: false,
-                message: 'please try again'
-            }
-        end
-    end
+#     def is_logged_in?
+#         if logged_in? && current_user
+#             render json: {
+#                 logged_in: true,
+#                 user: current_user
+#             }
+#         else
+#             render json: {
+#                 logged_in: false,
+#                 message: 'please try again'
+#             }
+#         end
+#     end
 
-    def destroy
-        logout!
-        render json: {
-            status: 200,
-            logged_out: true
-        }
-    end
+#     def destroy
+#         logout!
+#         render json: {
+#             status: 200,
+#             logged_out: true
+#         }
+#     end
 
-    private
+#     private
 
-    def session_params
-        params.require(:user).permit(:username, :email, :password)
-    end
+#     def session_params
+#         params.require(:user).permit(:username, :email, :password)
+#     end
 
 
-end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   resources :locations
   resources :groups
   resources :characters
+  resources :registrations, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
